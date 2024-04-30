142729ea6221fd32502bcf3a1b44ef8ab8be316c
sara <sara.pithion.simplon@gmail.com>
2024-04-30 15:42:25 +0200

    add symfony

---
+++ Symfony/src/Controller/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class  HomeController {
+    public function index(): Response {
+        return new Response("Hello World !");
+    }
+}
